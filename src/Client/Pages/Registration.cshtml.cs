d7774928c7e367406db48a4593ed706af91f6652
Don B. Stringham <donbstringham@icloud.com>
2022-03-02 20:11:34 -0700

    Added VSCode and new files for user registr...

--- src/Client/Pages/Registration.cshtml.cs
+++ src/Client/Pages/Registration.cshtml.cs
@@ -1,16 +1,25 @@
+using System;
 using BlabberApp.DataStore.Plugins;
 using BlabberApp.Domain.Common.Interfaces;
 using BlabberApp.Domain.Entities;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+namespace BlabberApp.Namespace
 {
     public class RegistrationModel : PageModel
     {
+        private User _user;
         public void OnGet()
-        {
+        { }
 
+        public void OnPostSubmit(UserModel usr)
+        {
+            _user = new User(usr.Username, usr.Email);
+            _user.FirstName = usr.FirstName;
+            _user.LastName = usr.LastName;
         }
     }
+
 }
---
+++ src/Client/Pages/UserModel.cs
@@ -0,0 +1,10 @@
+namespace BlabberApp.Namespace
+{
+    public class UserModel : PageModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
