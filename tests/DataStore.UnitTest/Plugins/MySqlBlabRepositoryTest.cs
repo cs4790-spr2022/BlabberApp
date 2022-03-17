b05ef478a13198fd05659daf057db2f3108ba095
Don B. Stringham <donbstringham@icloud.com>
2022-03-16 21:28:59 -0600

    Added new file.

--- tests/DataStore.UnitTest/Plugins/MySqlBlabRepositoryTest.cs
+++ tests/DataStore.UnitTest/Plugins/MySqlBlabRepositoryTest.cs
@@ -1,81 +1,81 @@
-using BlabberApp.DataStore.Plugins;
-using BlabberApp.Domain.Entities;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace BlabberApp.DataStore.UnitTests.Plugins
+using BlabberApp.DataStore.Plugins;
+using BlabberApp.Domain.Entities;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlabberApp.DataStore.UnitTests.Plugins;
+
+[TestClass]
+public class MySqlBlabRepositoryTest
 {
+    public MySqlBlabRepository h;
+    private string dsn = "server=143.110.159.170;uid=donstringham;pwd=letmein;database=donstringham";
 
-    [TestClass]
-    public class MySqlBlabRepositoryTest
+    [TestInitialize]
+    public void Setup()
     {
-        public MySqlBlabRepository h;
-        private string dsn = "server=143.110.159.170;uid=donstringham;pwd=letmein;database=donstringham";
+        h = new MySqlBlabRepository(dsn);
+    }
 
-        [TestInitialize]
-        public void Setup()
-        {
-            h = new MySqlBlabRepository(dsn);
-        }
+    [TestCleanup]
+    public void TearDown()
+    {
+        h.RemoveAll();
+    }
 
-        [TestCleanup]
-        public void TearDown()
-        {
-            h.RemoveAll();
-        }
+    [TestMethod]
+    public void MySqlBlabRepository_Instantiate()
+    {
+        // Arrange
+        // Act
+        MySqlBlabRepository a = new MySqlBlabRepository(dsn);
+        // Assert
+        Assert.AreEqual(h.ToString(), a.ToString());
+    }
 
-        [TestMethod]
-        public void MySqlBlabRepository_Instantiate()
+    [TestMethod]
+    public void MySqlBlabRepository_Connect_NoException()
+    {
+        // Arrange
+        MySqlBlabRepository a = new MySqlBlabRepository(dsn);
+        // Act and Assert
+        try
         {
-            // Arrange
-            // Act
-            MySqlBlabRepository a = new MySqlBlabRepository(dsn);
-            // Assert
-            Assert.AreEqual(h.ToString(), a.ToString());
+            a.Connect();
         }
+        catch (System.Exception ex)
+        {
+            Assert.Fail("Expected NO exception, but got: " + ex.Message);
+        }
+        Assert.IsTrue(true);
+    }
 
-        [TestMethod]
-        public void MySqlBlabRepository_Connect_NoException()
-        {
-            // Arrange
-            MySqlBlabRepository a = new MySqlBlabRepository(dsn);
-            // Act and Assert
-            try
-            {
-                a.Connect();
-            }
-            catch (System.Exception ex)
-            {
-                Assert.Fail("Expected NO exception, but got: " + ex.Message);
-            }
-            Assert.IsTrue(true);
-        }
+    [TestMethod]
+    public void MySqlBlabRepositoryTest_Connect_Exception()
+    {
+        // Arrange
+        string dsn = "server=143.110.159.170;uid=donstringham;pwd=letmein;database=blabber";
+        MySqlBlabRepository a = new MySqlBlabRepository(dsn);
+        // Act and Assert
+        Assert.ThrowsException<MySql.Data.MySqlClient.MySqlException>(() => a.Connect());
+    }
 
-        [TestMethod]
-        public void MySqlBlabRepositoryTest_Connect_Exception()
-        {
-            // Arrange
-            string dsn = "server=143.110.159.170;uid=donstringham;pwd=letmein;database=blabber";
-            MySqlBlabRepository a = new MySqlBlabRepository(dsn);
-            // Act and Assert
-            Assert.ThrowsException<MySql.Data.MySqlClient.MySqlException>(() => a.Connect());
-        }
-
-        [TestMethod]
-        public void MySqlBlabRepositoryTest_Insert()
-        {
-           // Arrange
-            Blab e = new Blab(
-                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet",
-                "foobar"
-            );
-           // Act
-           h.Add(e);
-           Blab a = h.GetById(e.Id);
-           // Assert
-           Assert.AreEqual(e.Id.ToString(), a.Id.ToString());
-           Assert.AreEqual(e.Content, a.Content);
-           Assert.AreEqual(e.Username, a.Username);
-        }
+    [TestMethod]
+    public void MySqlBlabRepositoryTest_Insert()
+    {
+        // Arrange
+        Blab e = new Blab(
+            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet",
+            "foobar"
+        );
+        // Act
+        h.Add(e);
+        Blab a = h.GetById(e.Id);
+        // Assert
+        Assert.AreEqual(e.Id.ToString(), a.Id.ToString());
+        Assert.AreEqual(e.Content, a.Content);
+        Assert.AreEqual(e.Username, a.Username);
     }
 }
